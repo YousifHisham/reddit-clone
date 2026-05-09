b35db0ece20c8679209a710ce066d2526a7caa8b
Michael-Maged <michaelmaged100@gmail.com>
2026-05-10 00:33:11 +0300

    debug: expose startup crash error in response

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,1 +1,9 @@
-module.exports = require('../src/server.js');
+try {
+  module.exports = require('../src/server.js');
+} catch (err) {
+  console.error('STARTUP CRASH:', err);
+  module.exports = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).json({ error: err.message, stack: err.stack });
+  };
+}
